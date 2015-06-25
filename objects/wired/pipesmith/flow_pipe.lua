1f6b5831c93c1356d9904f0d25603d405e9b8014
JoshuaNelson <joshua@nelsn.net>
2015-06-25 13:54:12 -0700

    Added IMPLY and NIMPLY logic gates, renamed opamp to buffer and general housekeeping.

--- objects/wired/pipesmith/flow_pipe.lua
+++ objects/wired/pipesmith/flow_pipe.lua
@@ -20,15 +20,15 @@ function output(state)
   end
 end
 
--- Pumps liquids from input pipes to current position.
+-- Pumps liquids from input pipe locations to current position.
 function pump()
   if storage.inbound == nil then
     return
   end
-  for pipeEntityId, pipeEntity in pairs(storage.inbound) do
-    local liquid = world.liquidAt(pipeEntity.position)
+  for targetId, targetEntity in pairs(storage.inbound) do
+    local liquid = world.liquidAt(targetEntity.position)
     if liquid then
-      world.destroyLiquid(pipeEntity.position)
+      world.destroyLiquid(targetEntity.position)
       world.spawnLiquid(entity.position(), liquid[1], liquid[2])
     end
   end
@@ -38,8 +38,6 @@ function update(dt)
   if entity.getInboundNodeLevel(0) then
     output(true)
     pump()
-  elseif entity.isOutboundNodeConnected(0) then
-    output(true)
   else
     output(false)
   end
@@ -49,7 +47,8 @@ function onNodeConnectionChange()
   storage.inbound = { }
   if entity.isInboundNodeConnected(1) then
     for targetId,val in pairs(entity.getInboundNodeIds(1)) do
-	  storage.inbound[targetId] = { ["position"] = world.entityPosition(targetId) }
-	end
+      --TODO Check if entity is pipe
+      storage.inbound[targetId] = { ["position"] = world.entityPosition(targetId) }
+    end
   end
 end
